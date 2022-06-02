4788de4ee97163d8f8fd1a13cd209c6d18566887
Jinwoo Jeong <jjw8967@ajou.ac.kr>
2022-06-02 00:08:59 -0400

    Init

---
+++ malloc.c
@@ -0,0 +1,118 @@
+/**********************************************************************
+ * Copyright (c) 2020
+ *  Jinwoo Jeong <jjw8967@ajou.ac.kr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTIABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ **********************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdint.h>
+
+#include "malloc.h"
+#include "types.h"
+#include "list_head.h"
+
+#define ALIGNMENT 32
+#define HDRSIZE sizeof(header_t)
+
+static LIST_HEAD(free_list); // Don't modify this line
+static algo_t g_algo;        // Don't modify this line
+static void *bp;             // Don't modify thie line
+
+/***********************************************************************
+ * extend_heap()
+ *
+ * DESCRIPTION
+ *   allocate size of bytes of memory and returns a pointer to the
+ *   allocated memory.
+ *
+ * RETURN VALUE
+ *   Return a pointer to the allocated memory.
+ */
+void *my_malloc(size_t size)
+{
+  /* Implement this function */
+  return NULL;
+}
+
+/***********************************************************************
+ * my_realloc()
+ *
+ * DESCRIPTION
+ *   tries to change the size of the allocation pointed to by ptr to
+ *   size, and returns ptr. If there is not enough memory block,
+ *   my_realloc() creates a new allocation, copies as much of the old
+ *   data pointed to by ptr as will fit to the new allocation, frees
+ *   the old allocation.
+ *
+ * RETURN VALUE
+ *   Return a pointer to the reallocated memory
+ */
+void *my_realloc(void *ptr, size_t size)
+{
+  /* Implement this function */
+  return NULL;
+}
+
+/***********************************************************************
+ * my_realloc()
+ *
+ * DESCRIPTION
+ *   deallocates the memory allocation pointed to by ptr.
+ */
+void my_free(void *ptr)
+{
+  /* Implement this function */
+  return;
+}
+
+/*====================================================================*/
+/*          ****** DO NOT MODIFY ANYTHING BELOW THIS LINE ******      */
+/*          ****** BUT YOU MAY CALL SOME IF YOU WANT TO.. ******      */
+/*          ****** EXCEPT TO mem_init() AND mem_deinit(). ******      */
+void mem_init(const algo_t algo)
+{
+  g_algo = algo;
+  bp = sbrk(0);
+}
+
+void mem_deinit()
+{
+  header_t *header;
+  size_t size = 0;
+  list_for_each_entry(header, &free_list, list) {
+    size += HDRSIZE + header->size;
+  }
+  sbrk(-size);
+
+  if (bp != sbrk(0)) {
+    fprintf(stderr, "[Error] There is memory leak\n");
+  }
+}
+
+void print_memory_layout()
+{
+  header_t *header;
+  int cnt = 0;
+
+  printf("===========================\n");
+  list_for_each_entry(header, &free_list, list) {
+    cnt++;
+    printf("%c %ld\n", (header->free) ? 'F' : 'M', header->size);
+  }
+
+  printf("Number of block: %d\n", cnt);
+  printf("===========================\n");
+  return;
+}
