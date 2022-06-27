42c1b80b627011c7650b0b3d690a46009c514c0b
Byeongsu Kang <raonsol@kakao.com>
2022-06-27 06:13:07 +0000

    ADD: coalescing for last free block

--- malloc.c
+++ malloc.c
@@ -30,7 +30,7 @@ static LIST_HEAD(free_list); // Don't modify this line
 static algo_t g_algo;        // Don't modify this line
 static void *bp;             // Don't modify thie line
 
-#define IS_LOGGING 1
+#define IS_LOGGING 0
 #define ALIGN(target, size) ((target - 1) / size * size + size)
 
 void print_memory_layout_console()
@@ -50,43 +50,12 @@ void print_memory_layout_console()
   return;
 }
 
-header_t *new_alloc(size_t size) {
-  header_t *p = sbrk(0); // get end of current heap
-  if(IS_LOGGING) fprintf(stderr, "Allocate %zu new bytes on %p\n", size, p);
 
-  // allocate new heap
-  // TODO: print error reason
-  if(sbrk(size+HDRSIZE)==(void*)-1) {
-      fprintf(stderr, "sbrk failed\n");
-      return NULL;
-  }
-
-  p->size = size;
-  p->free = false;
-
-  // add to the free_list
-  if (list_empty(&free_list))
-    INIT_LIST_HEAD(&free_list);
-  list_add_tail(&p->list, &free_list);
-
-  if(IS_LOGGING) fprintf(stderr, "End point: %p\n", sbrk(0));
-  return p;
-}
-
-header_t *split_free(header_t *p, size_t size){
-  if(IS_LOGGING) fprintf(stderr, "Split %p from %zu to %zu\n", p, p->size, size);
-  header_t *new_p = (header_t *)((char *)p + size);
-  new_p->size = p->size - size - HDRSIZE;
-  new_p->free = true;
-  list_add(&new_p->list, &p->list);
-  p->size = size;
-  return p;
-}
-
-void merge_prev(header_t *p){
+// return header_t because if it merges with prev, header also moves to the prev block
+header_t *merge_prev(header_t *p){
     if (p == list_first_entry(&free_list, header_t, list)){
         if (IS_LOGGING) fprintf(stderr, "Abort merge %p with %p(prev)\n", p, list_first_entry(&free_list, header_t, list));
-        return;
+        return p;
     }
 
     header_t *cur = list_prev_entry(p, list);
@@ -94,8 +63,10 @@ void merge_prev(header_t *p){
         if (IS_LOGGING) fprintf(stderr, "Merge %p with %p(prev)\n", p, cur);
         cur->size += p->size + HDRSIZE;
         list_del(&p->list);
-        merge_prev(cur);
-    }
+        cur = merge_prev(cur);
+        return cur;
+    } else
+        return p;
 }
 
 void merge_next(header_t *p){
@@ -112,10 +83,49 @@ void merge_next(header_t *p){
   }
 }
 
-void merge_free(header_t *p) {
+header_t *merge_free(header_t *p) {
   if (IS_LOGGING) fprintf(stderr, "Merge start %p\n", p);
+  header_t *new_p;
   merge_next(p);
-  merge_prev(p);
+  new_p=merge_prev(p);
+  return new_p;
+}
+
+header_t *new_alloc(size_t size) {
+  header_t *p = sbrk(0); // get end of current heap
+  if(IS_LOGGING) fprintf(stderr, "Allocate %zu new bytes on %p\n", size, p);
+
+  // allocate new heap
+  // TODO: print error reason
+  if(sbrk(size+HDRSIZE)==(void*)-1) {
+      fprintf(stderr, "sbrk failed\n");
+      return NULL;
+  }
+
+  p->size = size;
+  p->free = true;
+  // add to the free_list
+  if (list_empty(&free_list))
+    INIT_LIST_HEAD(&free_list);
+  list_add_tail(&p->list, &free_list);
+
+  // next block 할당 => prev free block과 merge => 앞의 포인터 기준으로 split
+  // coalesce free block
+  p = merge_free(p);
+  p->free = false;
+
+  if(IS_LOGGING) fprintf(stderr, "End point: %p\n", sbrk(0));
+  return p;
+}
+
+header_t *split_free(header_t *p, size_t size){
+  if(IS_LOGGING) fprintf(stderr, "Split %p from %zu to %zu\n", p, p->size, size);
+  header_t *new_p = (header_t *)((char *)p + size);
+  new_p->size = p->size - size - HDRSIZE;
+  new_p->free = true;
+  list_add(&new_p->list, &p->list);
+  p->size = size;
+  return p;
 }
 
 /***********************************************************************
@@ -215,7 +225,6 @@ void my_free(void *ptr)
   /* Implement this function */
   // TODO: check if *ptr is in the heap
   // TODO: increase size if last block is free
-  // TODO: merge free blocks recursively
   header_t *p = (header_t *)((char *)ptr - HDRSIZE);
   p->free = true;
   if(IS_LOGGING) fprintf(stderr, "Free block(size: %lu) on %p\n", p->size, p);
